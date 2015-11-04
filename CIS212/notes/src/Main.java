0e8028c05a3a07417256cf3edf45abe378499b9d
KrellTheHuman <KrellTheHuman@gmail.com>
2015-11-04 11:19:06 -0800

    assignment 5 created

--- CIS212/notes/src/Main.java
+++ CIS212/notes/src/Main.java
@@ -8,17 +8,17 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println(factorial(6));
-        System.out.println(fibonnaci(6));
+        System.out.println(fibonacci(6));
     }
 
     public static int factorial(int n) {
         return n != 1 ? n * factorial(n - 1) : 1;
     }
 
-    public static int fibonnaci(int n) {
+    public static int fibonacci(int n) {
         if (n <= 1) {
             return n;
         }
-        return fibonnaci(n - 1) + fibonnaci(n - 2);
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
